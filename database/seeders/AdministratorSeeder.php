f2ba189c7bc20d0f00375b4d3edd4fed7e2d71b0
= <rafaelpemaceti25@gmail.com>
2022-11-04 12:40:11 -0600

    Se agrego el template de AdminLTE

--- database/seeders/AdministratorSeeder.php
+++ database/seeders/AdministratorSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Administrator;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class AdministratorSeeder extends Seeder
 {
